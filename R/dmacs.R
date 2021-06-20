aecd24016f8eef8a5ee4e2e88b158adbe72e7db2
Hok Chio (Mark) Lai <marklhc@gmail.com>
2021-06-20 15:16:12 -0700

    Create dmacs_lavaan() to work with fitted objects
    dmacs() works without inputting loadings
    
    Force dmacs() to work with two groups
    
    dmacs() uses names from `loadings`

--- R/dmacs.R
+++ R/dmacs.R
@@ -31,42 +31,128 @@
 #' lambdar <- c(.8, .5, .4, .6)
 #' nuf <- c(0.1, 0, 0.2, 0)
 #' nur <- c(0.2, 0, 0, 0)
-#' dmacs(rbind(lambdaf, lambdar),
-#'       intercepts = rbind(nuf, nur),
+#' dmacs(rbind(nuf, nur),
+#'       loadings = rbind(lambdaf, lambdar),
 #'       pooled_item_sd = c(1, 1, 1, 1),
 #'       latent_mean = 0,
 #'       latent_sd = 1)
-dmacs <- function(loadings, intercepts, pooled_item_sd,
+dmacs <- function(intercepts, loadings = NULL, pooled_item_sd,
                   latent_mean = 0, latent_sd = 1) {
-  dloading <- diff(loadings)
+  if (nrow(intercepts) != 2) {
+    stop("Number of rows of loadings must be 2")
+  }
+  if (!is.null(loadings)) {
+    dloading <- diff(loadings)
+  } else {
+    dloading <- 0
+  }
   dintercept <- diff(intercepts)
   integral <- dintercept^2 + 2 * dintercept * dloading * latent_mean +
     dloading^2 * (latent_sd^2 + latent_mean^2)
-  c(sqrt(integral) / pooled_item_sd)
+  out <- sqrt(integral) / pooled_item_sd
+  if (!is.null(rownames(loadings))) {
+    rownames(out) <- paste(rownames(loadings), collapse = " vs ")
+  }
+  out
 }
 
-dmacs_pairwise <- function(loading_mat, intercept_mat, pooled_item_sd,
-                           latent_mean = 0, latent_var = 1) {
-  ngroups <- nrow(loading_mat)
-  ds <- combn(
-    ngroups,
-    m = 2,
+dmacs_pairwise <-
+  function(loading_mat,
+           intercept_mat,
+           pooled_item_sd,
+           latent_mean = 0,
+           latent_sd = 1) {
+    gp_names <- rownames(loading_mat)
+    if (is.null(gp_names)) {
+      gp_names <- seq_len(nrow(loading_mat))
+    }
+    ds <- combn(
+      gp_names,
+      m = 2,
+      FUN = function(x) {
+        dmacs(
+          loading_mat[x,],
+          intercept_mat[x,],
+          pooled_item_sd = pooled_item_sd,
+          latent_mean = latent_mean,
+          latent_sd = latent_sd
+        )
+      },
+      simplify = FALSE
+    )
+    ds <- do.call(rbind, ds)
+    rownames(ds) <- combn(gp_names,
+                          m = 2,
+                          FUN = paste,
+                          collapse = " vs ")
+    colnames(ds) <- colnames(intercept_mat)
+    ds
+  }
+
+# Function to extract specific parameters
+getpt <- function(pt, type = c("load", "int", "uniq", "equality"),
+                  ind_names) {
+  # ind_names <- object@pta$vnames$ov.ind[[1]]
+  rowids <- numeric(0)
+  type <- match.arg(type, several.ok = TRUE)
+  if ("load" %in% type) {
+    rowids <- c(rowids, which(pt$op == "=~" & pt$rhs %in% ind_names))
+  }
+  if ("int" %in% type) {
+    rowids <- c(rowids, which(pt$op == "~1" & pt$lhs %in% ind_names))
+  }
+  if ("uniq" %in% type) {
+    rowids <- c(rowids, which(pt$op == "~~" & pt$lhs %in% ind_names &
+                                pt$lhs == pt$rhs))
+  }
+  if ("equality" %in% type) {
+    rowids <- c(rowids, which(pt$op == "=="))
+  }
+  pt[rowids, ]
+}
+# Compute pooled SD
+pooledvar <- function(vars, ns) {
+  vars <- matrix(vars, ncol = length(ns))
+  apply(vars, MARGIN = 1, weighted.mean, w = ns - 1)
+}
+
+dmacs_lavaan <- function(object) {
+  stopifnot(lavInspect(object, what = "ngroups") == 2)
+  pt <- lavaan::parTable(object)
+  ind_names <- object@pta$vnames$ov.ind[[1]]
+  pt_par <- getpt(pt, type = c("load", "int"), ind_names = ind_names)
+  pt_eq <- getpt(pt, type = "equality", ind_names = ind_names)
+  ninv_par <- setdiff(pt_par$plabel, unique(c(pt_eq$lhs, pt_eq$rhs)))
+  ninv_ov <-
+    unique(unlist(subset(pt_par, plabel %in% ninv_par)[c("lhs", "rhs")]))
+  ninv_ov <- intersect(ninv_ov, ind_names)
+  pars <- lavaan::lavInspect(object, what = "est")
+  sampstat <- lavInspect(object, "sampstat")
+  vars <- vapply(sampstat, function(x) diag(x$cov)[ninv_ov],
+                 FUN.VALUE = numeric(length(ninv_ov)))
+  ns <- lavInspect(object, "nobs")
+  pooled_item_sd <- sqrt(pooledvar(vars, ns))
+  intercept_mat <- lapply(
+    pars,
     FUN = function(x) {
-      dmacs(
-        loading_mat[x,],
-        intercept_mat[x,],
-        pooled_item_sd = pooled_item_sd,
-        latent_mean = latent_mean,
-        latent_var = latent_var
-      )
-    },
-    simplify = FALSE
+      x$nu[ninv_ov, ]
+    }
   )
-  ds <- do.call(rbind, ds)
-  rownames(ds) <- combn(ngroups,
-                         m = 2,
-                         FUN = paste,
-                         collapse = " vs ")
-  colnames(ds) <- colnames(loading_mat)
-  ds
+  intercept_mat <- do.call(rbind, intercept_mat)
+  loading_mat <- lapply(
+    pars,
+    FUN = function(x) {
+      sel_lambda <- x$lambda[ninv_ov, , drop = FALSE]
+      dm <- dimnames(sel_lambda)
+      out <- c(sel_lambda)
+      names(out) <- outer(dm[[1]], dm[[2]], FUN = paste, sep = "-")
+      out
+    }
+  )
+  loading_mat <- do.call(rbind, loading_mat)
+  num_lvs <- length(object@pta$vnames$lv[[1]])
+  intercept_mat <- t(rep(1, num_lvs)) %x% intercept_mat
+  dmacs(intercepts = intercept_mat,
+        loadings = loading_mat,
+        pooled_item_sd = rep(pooled_item_sd, num_lvs))
 }
