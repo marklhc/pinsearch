939f0e67d528e222e596c742d24baaedccd7512e
Hok Chio (Mark) Lai <marklhc@gmail.com>
2021-07-31 17:56:36 -0700

    Add new test for more than two groups

--- tests/testthat/test-pinSearch.R
+++ tests/testthat/test-pinSearch.R
@@ -44,3 +44,16 @@ test_that("pinSearch() works properly for noninvariant intercepts", {
   expect_identical(ps3[[2]]$type, rep(c("loadings", "intercepts"), each = 2))
 })
 
+test_that("pinSearch() works properly for three groups", {
+  cov2 <- tcrossprod(lambda2) * 1.3 + diag(.5, 5)
+  dimnames(cov2) <- list(paste0("y", 1:5), paste0("y", 1:5))
+  mean2 <- lambda2 * .4 + nu2
+  ps3 <- pinSearch(' f =~ y1 + y2 + y3 + y4 + y5',
+                   sample.cov = list(cov1, cov2),
+                   sample.mean = list(mean1, mean2),
+                   sample.nobs = c(10000, 10000),
+                   type = "intercepts")
+  expect_identical(ps3[[2]]$item, c("y1", "y2", "y5", "y4"))
+  expect_identical(ps3[[2]]$type, rep(c("loadings", "intercepts"), each = 2))
+})
+
