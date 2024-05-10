608b5e4144e1ea088ead2d89a1ba9bda0cb42e2d
Hok Chio (Mark) Lai <marklhc@gmail.com>
2024-05-10 12:25:31 -0700

    Update documentation

---
+++ R/data.R
@@ -0,0 +1,18 @@
+#' Simulated data with 15 items of an Alcohol Belief Scale
+#'
+#' Simulation based on fitting a configural invariance model to
+#'   data made public by Lui (2019,
+#'   <https://psycnet.apa.org/record/2019-08221-001>). The original data
+#'   can be found at <https://osf.io/wxjsg>.
+#'
+#' @format ## `lui_sim`
+#' A data frame with 910 rows and 16 columns:
+#' \describe{
+#'   \item{class1 to class15}{Binary items}
+#'   \item{group}{six groups: 1 = White Male, 2 = Asian Male,
+#'                            3 = Hispanic Male, 4 = White Female,
+#'                            5 = Asian Female, 6 = Hispanic Female}
+#' 
+#' }
+#' @source <https://osf.io/wxjsg>
+"lui_sim"
